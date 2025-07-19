eae45274bfb8f83bc776cfb92efe855e7adffb09
Aram m. <aramma1990@gmail.com>
2025-07-19 23:46:40 +0400

    branch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rotate-grid/' 
 })
